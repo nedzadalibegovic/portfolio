d616f190656a17b609d485d446270f00e50db8b1
Nedzad Alibegovic <git@nedzad.dev>
2020-04-24 04:12:45 +0200

    Shuffle image array before sending to client

--- api/routes/images.js
+++ api/routes/images.js
@@ -1,11 +1,25 @@
-const express = require('express');
+const router = require('express').Router();
 const Image = require('../models/image');
 
-const router = express.Router();
+// https://stackoverflow.com/a/6274398
+const shuffle = (array) => {
+    let counter = array.length;
+
+    while (counter > 0) {
+        const index = Math.floor(Math.random() * counter);
+        const temp = array[--counter];
+
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+};
 
 router.get('/', async (req, res) => {
     try {
-        let images = await Image.find();
+        const images = await Image.find();
+        shuffle(images);
         res.status(200).json(images);
     } catch (err) {
         res.status(500).json({ message: err });
@@ -14,7 +28,7 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
     try {
-        let found = await Image.findById(req.params.id);
+        const found = await Image.findById(req.params.id);
         res.status(200).json(found);
     } catch (err) {
         res.status(500).json({ message: err });
