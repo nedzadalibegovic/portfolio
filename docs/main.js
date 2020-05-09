e26ff43fcea6202aead5a5d86f408d60677e4417
Nedzad Alibegovic <nedzad@nedzad.dev>
2020-05-09 15:28:38 +0200

    Use MongoDB Stitch for serverless API

--- docs/main.js
+++ docs/main.js
@@ -19,10 +19,28 @@ const addOnClick = () => {
     });
 };
 
+const shuffle = (array) => {
+    let counter = array.length;
+
+    while (counter > 0) {
+        const index = Math.floor(Math.random() * counter);
+        const temp = array[--counter];
+
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+};
+
 const main = async () => {
-    const response = await fetch('https://portfolio.lyra.nedzad.dev/');
+    const response = await fetch(
+        'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/portfolio-qcvuk/service/portfolio/incoming_webhook/api'
+    );
     const json = await response.json();
     const images = json.map((image) => image.url);
+
+    shuffle(images);
 
     for (image of images) {
         $('.gallery').append(`<img class="gallery-item" src="${image}"/>`);
