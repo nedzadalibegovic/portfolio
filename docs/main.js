3aa42e17c1e969e11beb681d9d2b0ae148dc14eb
Nedzad Alibegovic <nedzad@nedzad.dev>
2020-09-27 21:05:34 +0200

    Change tab size

--- docs/main.js
+++ docs/main.js
@@ -1,38 +1,38 @@
 (async () => {
-    const response = await fetch(
-        'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/portfolio-qcvuk/service/portfolio/incoming_webhook/api'
-    );
-    const json = await response.json();
+  const response = await fetch(
+    "https://webhooks.mongodb-stitch.com/api/client/v2.0/app/portfolio-qcvuk/service/portfolio/incoming_webhook/api"
+  );
+  const json = await response.json();
 
-    if (!response.ok) return;
+  if (!response.ok) return;
 
-    const gallery = document.querySelector('.gallery');
-    const overlayBg = document.querySelector('.overlay-background');
-    const overlay = document.querySelector('.overlay');
+  const gallery = document.querySelector(".gallery");
+  const overlayBg = document.querySelector(".overlay-background");
+  const overlay = document.querySelector(".overlay");
 
-    overlayBg.addEventListener('click', () => {
-        overlayBg.classList.add('overlay-background-hide');
+  overlayBg.addEventListener("click", () => {
+    overlayBg.classList.add("overlay-background-hide");
 
-        setTimeout(() => {
-            overlayBg.classList.remove('overlay-background-hide');
-            overlayBg.setAttribute('style', 'display: none');
-            overlay.removeChild(overlay.firstChild);
-        }, 300);
-    });
+    setTimeout(() => {
+      overlayBg.classList.remove("overlay-background-hide");
+      overlayBg.setAttribute("style", "display: none");
+      overlay.removeChild(overlay.firstChild);
+    }, 300);
+  });
 
-    for (const doc of json) {
-        const galleryImage = document.createElement('img');
+  for (const doc of json) {
+    const galleryImage = document.createElement("img");
 
-        galleryImage.src = doc.url;
-        galleryImage.className = 'gallery-item';
-        galleryImage.onclick = () => {
-            const overlayImage = document.createElement('img');
-            overlayImage.src = doc.url;
+    galleryImage.src = doc.url;
+    galleryImage.className = "gallery-item";
+    galleryImage.onclick = () => {
+      const overlayImage = document.createElement("img");
+      overlayImage.src = doc.url;
 
-            overlay.appendChild(overlayImage);
-            overlayBg.setAttribute('style', 'display: flex');
-        };
+      overlay.appendChild(overlayImage);
+      overlayBg.setAttribute("style", "display: flex");
+    };
 
-        gallery.appendChild(galleryImage);
-    }
+    gallery.appendChild(galleryImage);
+  }
 })();
